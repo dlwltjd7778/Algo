055985382d3c87a12290cc16c8f79a173280fcd0
dlwltjd7778 <dlwltjd7778@gmail.com>
2020-08-12 00:07:42 +0900

    bj_1260 ok >> graph를 dfs,bfs로 풀기

--- Algo/src/BaekJoon/bj_1260_graph.java
+++ Algo/src/BaekJoon/bj_1260_graph.java
@@ -17,7 +17,6 @@ public class bj_1260_graph {
 	static StringTokenizer st;
 	static BufferedWriter bw;
 	static int N,M,V;
-	static ArrayList<InfoM> list;
 	static boolean[] visit;
 	public static void main(String[] args) throws IOException {
 		
@@ -28,73 +27,82 @@ public class bj_1260_graph {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		V = Integer.parseInt(st.nextToken());
-		
-		list = new ArrayList<InfoM>();
+		ArrayList<Integer>[] list = new ArrayList[N+1];
+		for(int i=0;i<N+1;i++) {
+			list[i] = new ArrayList<Integer>();
+		}
 		visit = new boolean[N+1];
 		
 		for(int i=0;i<M;i++) {
 			st = new StringTokenizer(br.readLine()," "); 
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			list.add(new InfoM(start, end));
-			list.add(new InfoM(end, start));
+			list[start].add(end);
+			list[end].add(start);
 		} // input end
 		
-		Collections.sort(list);		// 순서대로 방문하기 위해 정렬
+		for(int i=0;i<N+1;i++) {
+			Collections.sort(list[i]); // 순서대로 방문하기 위해 정렬
+		}
+//		for(int i=0;i<N+1;i++) {
+//			for(int j=0;j<list[i].size();j++) {
+//				System.out.print(list[i].get(j) + " ");
+//			}
+//			System.out.println();
+//		}
+		
+		dfs(V,0,list);
+		bw.newLine();
+		bfs(V, list);
+				
+		bw.flush();
+		
+	}
+	
+	static void dfs(int start, int count, ArrayList<Integer>[] list) throws IOException {
 		
 		
+		visit[start] = true;
+		bw.write(start + " ");
 		
+		if(count==M) return;
 		
-		
+		for(int i=0;i<list[start].size();i++) {
+			int tempEnd = list[start].get(i);
+			if(!visit[tempEnd]) {
+				visit[tempEnd] = true;
+				dfs(tempEnd,count+1,list);
+			}
+			
+		}
 		
 		
 	}
 	
-	static void bfs(int start) throws IOException {
+	static void bfs(int start, ArrayList<Integer>[] list) throws IOException {
 		visit = new boolean[N+1];		// 초기화
 		Queue<Integer> queue = new LinkedList<Integer>();
 		
 		visit[start] = true;
 		queue.add(start);
-		bw.write(start + " ");
 		
 		while(!queue.isEmpty()) {
 			int size = queue.size();
 			for(int s=0;s<size;s++) {
 				int temp = queue.poll();
 				bw.write(temp + " ");
-				
-				if(!visit[temp])
-				
-				
-				
+
+				for(int i=0;i<list[temp].size();i++) {
+					int tempEnd = list[temp].get(i);
+					if(!visit[tempEnd]) {
+						queue.add(tempEnd);
+						visit[tempEnd] = true;
+					}
+				}
 			}
 		}
-		
-		
-		
-		
-	}
+	} // bfs end
 	
 	
-	// 간선 정보를 저장하는 클래스
-	static class InfoM implements Comparable<InfoM>{ 
-		int start,end;
-		public InfoM(int start,int end) {
-			this.start = start;
-			this.end = end;
-		}
-		@Override
-		public int compareTo(InfoM o) {
-			if(this.start == o.start) {
-				return this.end - o.end;
-			}
-			return this.start - o.start;
-		}
-		@Override
-		public String toString() {
-			// TODO Auto-generated method stub
-			return start + " " + end;
-		}
-	}
+	
 }
