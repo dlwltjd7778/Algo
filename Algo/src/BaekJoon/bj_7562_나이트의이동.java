ff2a6cf0be8758ba458480120226e2bf4213c29a
dlwltjd7778 <dlwltjd7778@gmail.com>
2020-08-15 00:03:14 +0900

    bj_7562 ok

--- Algo/src/BaekJoon/bj_7562_나이트의이동.java
+++ Algo/src/BaekJoon/bj_7562_나이트의이동.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class bj_7562_나이트의이동 {
 
-	
-	static int TC, I;	// 테스트 케이스 수, 체스판 크기
-	static Queue<Point> queue;
-	
 	static int[] di = {-2,-1,1,2,-2,2,-1,1};
 	static int[] dj = {1,2,2,1,-1,-1,-2,-2};
-	static int si,sj,ei,ej;	//  시작좌표, 끝좌표
-	static int count;
+	
+	static int TC, I;	// 테스트 케이스 수, 체스판 크기
+	static Queue<Point> queue;	
+	static boolean[][] visit;		// 방문 체크할 배열
+	static int si,sj,ei,ej;			//  시작좌표, 끝좌표
+	
 	
 	public static void main(String[] args) {
 		
@@ -23,24 +23,30 @@ public class bj_7562_나이트의이동 {
 		for(int tc=1;tc<=TC;tc++) {
 			
 			I = sc.nextInt();
-			si = sc.nextInt();
-			sj = sc.nextInt();
-			ei = sc.nextInt();
-			ej = sc.nextInt();
-			count = 0;
+			si = sc.nextInt();		// 시작 i ( start i )
+			sj = sc.nextInt();		// 시작 j ( start j )
+			ei = sc.nextInt();		// 끝 i ( end i )
+			ej = sc.nextInt();		// 끝 j ( end j )
+			// input end
 			
-			////////input end
-			
+			visit = new boolean[I][I];
 			queue = new LinkedList<Point>();
-			queue.add(new Point(si, sj,count));
-			func();
+			queue.add(new Point(si, sj));
 
+			boolean goBfs = true;	// bfs함수 진행할것인가..
+			if(si==ei && sj==ej) {
+				System.out.println(0);
+				goBfs = false;
+			}
 			
 			
-		}
-		
-	}
-	static void func() {
+			if(goBfs) bfs();
+			
+		}	// tc end
+	} // main end
+	static void bfs() {
+		int cnt = 0;
+		boolean chk = false;
 		while(!queue.isEmpty()) {
 			int size = queue.size();
 			for(int s=0;s<size;s++) {
@@ -48,30 +54,32 @@ public class bj_7562_나이트의이동 {
 				
 				int pi = p.i;
 				int pj = p.j;
+				
 				for(int d=0;d<8;d++) {
 					int ni = pi + di[d];
 					int nj = pj + dj[d];
 					
-					if(ni>=0 && nj>=0 && ni<I && nj<I) {
-						if(ni==ei && nj==ej) {
-							System.out.println(p.count+1);
+					if(ni>=0 && nj>=0 && ni<I && nj<I && !visit[ni][nj]) {	// 범위 체크
+						if(ni==ei && nj==ej) {		  // 움직인 곳이 원하는 좌표면
+							System.out.println(cnt+1);
 							return;
 						}
-						queue.add(new Point(ni, nj,p.count+1));
+						queue.add(new Point(ni, nj)); // 아니면 q에넣기
+						visit[ni][nj] = true;		  // 방문처리 하기
 					}
 				}
 				
 			}
+			cnt++;
 			
 		}
-		System.out.println(0);
+		System.out.println(cnt);
 	}
 	static class Point {
-		int i,j,count;
-		Point(int i,int j, int count){
+		int i,j;
+		Point(int i,int j){
 			this.i = i;
 			this.j = j;
-			this.count = count;
 		}
 		
 	}
