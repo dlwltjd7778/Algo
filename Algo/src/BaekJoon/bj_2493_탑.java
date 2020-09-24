015187e4eafd030eb362327eea19a438ba15e2c4
dlwltjd7778 <dlwltjd7778@gmail.com>
2020-09-25 02:50:08 +0900

    bj_2493_탑

--- Algo/src/BaekJoon/bj_2493_탑.java
+++ Algo/src/BaekJoon/bj_2493_탑.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class bj_2493 {
+public class bj_2493_탑 {
 
 	public static void main(String[] args) throws IOException {
 
--- Algo/src/BaekJoon/bj_2493_탑_ans.java
+++ Algo/src/BaekJoon/bj_2493_탑_ans.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class bj_2493_ans {
+public class bj_2493_탑_ans {
 
 	public static void main(String[] args) throws IOException {
 
