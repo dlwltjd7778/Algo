3e51802cf4c7b83022e1c1381d7793dff4690bef
dlwltjd7778 <dlwltjd7778@gmail.com>
2020-10-04 02:35:05 +0900

    pg_스택큐_다리를지나는트럭

--- Algo/src/programmers/pg_스택큐_다리를지나는트럭.java
+++ Algo/src/programmers/pg_스택큐_다리를지나는트럭.java
@@ -15,23 +15,36 @@ public class pg_스택큐_다리를지나는트럭 {
         }
         
         int nowW = 0;	// 현재 다리위의 트럭 무게
+        bridgeQ.add(truckQ.peek());
+        nowW += truckQ.poll();
         
-        for(int i=1;i<=bridge_length;i++) {
-
-        	if(nowW<=weight) {
-        		truckQ.add(truckQ.peek());
-        		nowW += truckQ.poll();
+        while(true) {
+        	answer++;
+        	
+        	if(truckQ.isEmpty()) {
+        		answer += bridge_length;
+        		break;
         	}
         	
+    		if(!truckQ.isEmpty() && truckQ.peek()+nowW <= weight) {	// 만약 트럭Q의 피크값과 현재 트럭무게 더한값이 다리가 견디는 무게라면
+    			bridgeQ.add(truckQ.peek());
+    			nowW += truckQ.poll();
+    		} else {
+    			bridgeQ.add(0);
+    		}
+    		
+    		if(bridgeQ.size()==bridge_length) {
+    			nowW -= bridgeQ.poll();
+    		} 
         }
-        
         
         return answer;
     }
 	public static void main(String[] args) {
-		int bridge_length = 2;
-		int weight = 10;
-		int[] truck_weights = {7,4,5,6};
+
+		int bridge_length = 100;
+		int weight = 100;
+		int[] truck_weights = {10};
 		System.out.println(solution(bridge_length, weight, truck_weights));
 	}
 }
