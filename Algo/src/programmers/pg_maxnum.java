7fca3aac073a74e1b78b58e4e65522f4635dd28e
jiseong.lee <jiseong.lee@bespinglobal.com>
2022-04-08 00:27:01 +0900

    programmers 가장 큰 수

---
+++ Algo/src/programmers/pg_maxnum.java
@@ -0,0 +1,46 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 정렬 - 가장큰수
+ * https://programmers.co.kr/learn/courses/30/lessons/42746?language=java
+ */
+
+public class pg_maxnum {
+
+    public static void main(String[] args) {
+        int[] numbers = {6, 10, 2};
+        solution(numbers);
+    }
+
+    public static String solution(int[] numbers) {
+        String answer = "";
+
+        String[] numString = new String[numbers.length];
+        for (int i=0;i<numbers.length;i++) {
+            numString[i] = Integer.toString(numbers[i]);
+        }
+
+        Arrays.sort(numString, new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        if(numString[0].equals("0")) return "0";
+
+        for (String num :numString) {
+            answer += num;
+        }
+
+        return answer;
+    }
+
+
+
+}
+
+
