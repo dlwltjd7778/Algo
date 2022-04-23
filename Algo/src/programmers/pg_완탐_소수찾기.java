551d8ccacfb10f72edb9a17f2b744e5ac1df30c9
dlwltjd7778 <dlwltjd7778@naver.com>
2022-04-23 23:20:23 +0900

    프로그래머스 소수찾기

--- Algo/src/programmers/pg_완탐_소수찾기.java
+++ Algo/src/programmers/pg_완탐_소수찾기.java
@@ -1,34 +1,55 @@
 package programmers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class pg_완탐_소수찾기 {
 
 	public static void main(String[] args) {
-		
+		solution("17");
 	}
-	
-	class Solution {
-		int num[];
-		boolean[] select;
-	    public int solution(String numbers) {
-	        int answer = 0;
-	        
-	        num = new int[numbers.length()];
-	        select = new boolean[numbers.length()];
-	        
-	        for(int i=0;i<numbers.length();i++) {
-	        	num[i] = numbers.charAt(i)-'0';
-	        }
-	        
-	        
-	        
-	        
-	        
-	        
-	        return answer;
-	    }
-	    
-	    public void powerSet() {
-	    	
-	    }
+
+	static Set<Integer> set;
+
+	public static int solution(String numbers) {
+		int answer = 0;
+		set = new HashSet<>();
+
+		select("", numbers);
+
+		for (int a: set) {
+			if(isPrimeNum(a)) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+		return answer;
+	}
+
+	public static void select(String now, String number) {
+		if (!"".equals(now)) {
+			set.add(Integer.parseInt(now));
+		}
+
+		for (int i=0;i<number.length();i++) {
+			select(now + number.charAt(i), number.substring(0,i) + number.substring(i+1));
+		}
+	}
+
+	public static boolean isPrimeNum(int num) {
+
+		if (num==0 || num==1) {
+			return false;
+		}
+
+		int limit = (int)Math.sqrt(num);
+
+		for (int i=2; i<=limit; i++) {
+			if(num%i==0) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
